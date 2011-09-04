6df96c60957f9079966fb2163430aa4bc092f243
Thomas Rabaix <thomas.rabaix@ekino.com>
2011-09-04 12:54:32 +0200

    fix knplab menu bundle changes

--- app/autoload.php
+++ app/autoload.php
@@ -5,7 +5,10 @@ use Doctrine\Common\Annotations\AnnotationRegistry;
 
 $loader = new UniversalClassLoader();
 $loader->registerNamespaces(array(
-    'Symfony'           => array(__DIR__.'/../vendor/symfony/src', __DIR__.'/../vendor/bundles'),
+    'Symfony'           => array(
+        __DIR__.'/../vendor/symfony/src', 
+        __DIR__.'/../vendor/bundles'
+    ),
     'Sensio'            => __DIR__.'/../vendor/bundles',
     'JMS'               => __DIR__.'/../vendor/bundles',
     'Doctrine\\Common'  => __DIR__.'/../vendor/doctrine-common/lib',
@@ -15,7 +18,10 @@ $loader->registerNamespaces(array(
     'Assetic'           => __DIR__.'/../vendor/assetic/src',
     'Metadata'          => __DIR__.'/../vendor/metadata/src',
     'FOS'               => __DIR__.'/../vendor/bundles',
-    'Knp'               => __DIR__.'/../vendor/bundles',
+    'Knp'               => array(
+        __DIR__.'/../vendor/bundles',
+        __DIR__.'/../knp/menu/src'
+    ),
     'Sonata'            => __DIR__.'/../vendor/bundles',
     'Imagine'           => __DIR__.'/../vendor/imagine/lib',
     'Gaufrette'         => __DIR__.'/../vendor/gaufrette/src',
