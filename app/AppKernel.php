cf258fde9e22c04b0c57dcaa8e257c6c678076b3
Thomas Rabaix <thomas.rabaix@ekino.com>
2011-12-16 10:38:29 +0100

    Integrated the LiipImagineBundle (disable by default)

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -43,6 +43,7 @@ class AppKernel extends Kernel
             // MEDIA
             new Sonata\MediaBundle\SonataMediaBundle(),
             new Application\Sonata\MediaBundle\ApplicationSonataMediaBundle(),
+            // new Liip\ImagineBundle\LiipImagineBundle(),
 
             // SONATA CORE & HELPER BUNDLES
             new Sonata\EasyExtendsBundle\SonataEasyExtendsBundle(),
