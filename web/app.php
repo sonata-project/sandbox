eee8dd23607afe0c0062468eef5cf94c6c7b1501
Thomas Rabaix <thomas.rabaix@ekino.com>
2012-01-30 16:25:14 +0100

    Fix double request declaration

--- web/app.php
+++ web/app.php
@@ -5,8 +5,6 @@ require_once __DIR__.'/../app/AppKernel.php';
 //require_once __DIR__.'/../app/bootstrap_cache.php.cache';
 //require_once __DIR__.'/../app/AppCache.php';
 
-use Symfony\Component\HttpFoundation\Request;
-
 //$kernel = new AppCache(new AppKernel('prod', false));
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
