84cbef613a3a5bcb04f541c4a27d3e666749077e
Hugo Briand <briand@ekino.com>
2013-06-20 17:48:42 +0200

    SONATA-20 - Reflecting ecommerce lib refactoring

--- src/Sonata/Bundle/EcommerceDemoBundle/Controller/HardwareController.php
+++ src/Sonata/Bundle/EcommerceDemoBundle/Controller/HardwareController.php
@@ -4,6 +4,8 @@ namespace Sonata\Bundle\EcommerceDemoBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sonata\ProductBundle\Controller\BaseProductController;
+use Sonata\Component\Basket\BasketElementInterface;
+use Sonata\Component\Order\OrderElementInterface;
 
 /**
  *
@@ -13,4 +15,43 @@ use Sonata\ProductBundle\Controller\BaseProductController;
  */
 class HardwareController extends BaseProductController
 {
+    /**
+     * {@inheritDoc}
+     */
+    public function viewVariationsAction($productId, $slug)
+    {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function viewBasketElement(BasketElementInterface $basketElement)
+    {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function viewBasketElementConfirmation(BasketElementInterface $basketElement)
+    {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function viewOrderElement(OrderElementInterface $orderElement)
+    {
+
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function viewEditOrderElement(OrderElementInterface $orderElement)
+    {
+
+    }
 }
