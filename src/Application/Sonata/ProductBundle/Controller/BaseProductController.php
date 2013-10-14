1c1d181fcec5ceab198ba3c5cc690d46f5e4c88e
Xavier Coureau <xav.is@2cool4school.fr>
2013-10-14 16:08:49 +0200

    SONATA-110 - Added Opengraph data for product sheet

--- src/Application/Sonata/ProductBundle/Controller/BaseProductController.php
+++ src/Application/Sonata/ProductBundle/Controller/BaseProductController.php
@@ -3,8 +3,6 @@
 namespace Application\Sonata\ProductBundle\Controller;
 
 use Sonata\ProductBundle\Controller\BaseProductController as BaseController;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
-use Symfony\Component\HttpFoundation\Response;
 
 abstract class BaseProductController extends BaseController
 {
@@ -13,21 +11,44 @@ abstract class BaseProductController extends BaseController
      *
      * @param \Sonata\Component\Product\ProductInterface $product
      *
-     * @throws NotFoundHttpException
+     * @throws \Symfony\Component\HttpKernel\Exception\NotFoundHttpException
      *
-     * @return Response
+     * @return \Symfony\Component\HttpFoundation\Response
      */
     public function viewAction($product)
     {
         $view = parent::viewAction($product);
+        $seoPage = $this->container->get('sonata.seo.page');
+        $currency = $this->get('sonata.price.currency.detector')->getCurrency();
 
-        $this->container->get('sonata.seo.page')
+        $seoPage
             // Twitter metadata
             ->addMeta('name', 'twitter:card', 'product')
             ->addMeta('name', 'twitter:title', $product->getName())
             ->addMeta('name', 'twitter:description', $product->getDescription())
             ->addMeta('name', 'twitter:label1', 'Price')
-            ->addMeta('name', 'twitter:data1', sprintf('%.2f%s', $product->getPrice(), $this->get('sonata.price.currency.detector')->getCurrency()));
+            ->addMeta('name', 'twitter:data1', sprintf('%.2f%s', $product->getPrice(), $currency))
+
+            // Facebook OpenGraph metadata
+            ->addMeta('property', 'og:type', 'og:product')
+            ->addMeta('property', 'og:title', $product->getName())
+            ->addMeta('property', 'og:description', $product->getDescription())
+            ->addMeta('property', 'product:price:amount', $product->getPrice())
+            ->addMeta('property', 'product:price:currency', $currency)
+            ->addMeta('property', 'og:url', $this->getRequest()->getUri())
+            ;
+
+        // If a media is available, we add the opengraph image data
+        if ($image = $product->getImage()) {
+            $format = 'reference';
+            $provider = $this->get('sonata.media.pool')
+                ->getProvider($image->getProviderName());
+
+            $seoPage->addMeta('property', 'og:image:type', $provider->generatePublicUrl($image, $format))
+                ->addMeta('property', 'og:image:width', $image->getWidth())
+                ->addMeta('property', 'og:image:height', $image->getHeight());
+        }
+
 
         return $view;
     }
