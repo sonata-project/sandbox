7916aa154f389eff2e0b675795692966dfec5ba4
Hugo Briand <briand@ekino.com>
2013-10-14 18:40:08 +0200

    Fixed typo on load_data

--- load_data.php
+++ load_data.php
@@ -57,7 +57,7 @@ execute_commands(array(
     'app/console cache:create-cache-class --env=dev',
     'app/console doctrine:database:drop --force',
     'app/console doctrine:database:create',
-//    'app/console doctrine:schema:update --force',
+    'app/console doctrine:schema:update --force',
     'app/console doctrine:fixtures:load --verbose',
     'app/console sonata:page:update-core-routes --site=all',
     'app/console sonata:page:create-snapshots --site=all',
