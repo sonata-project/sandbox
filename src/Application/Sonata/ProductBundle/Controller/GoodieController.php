227d0f965ce67753a68f4ac566a5ee85e186bf4a
Xavier Coureau <xav.is@2cool4school.fr>
2013-10-24 12:46:56 +0200

    SONATA-110 - SONATA-112 - Refactoring for demo

--- src/Application/Sonata/ProductBundle/Controller/GoodieController.php
+++ src/Application/Sonata/ProductBundle/Controller/GoodieController.php
@@ -11,6 +11,8 @@
 
 namespace Application\Sonata\ProductBundle\Controller;
 
+use Sonata\ProductBundle\Controller\BaseProductController;
+
 /**
  * Overwrite methods from the BaseProductController if you want to change the behavior
  * for the current product
