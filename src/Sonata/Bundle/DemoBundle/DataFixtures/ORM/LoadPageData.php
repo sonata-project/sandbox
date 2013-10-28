395bef8a41ad6b1f24db47d3e0dcc8981ab0ff24
Xavier Coureau <xav.is@2cool4school.fr>
2013-10-28 11:26:21 +0100

    SONATA-106 - Removed footer block and implemented new footer

--- src/Sonata/Bundle/DemoBundle/DataFixtures/ORM/LoadPageData.php
+++ src/Sonata/Bundle/DemoBundle/DataFixtures/ORM/LoadPageData.php
@@ -455,39 +455,6 @@ CONTENT
         $menu->setEnabled(true);
         $menu->setPage($global);
 
-        $global->addBlocks($footer = $blockInteractor->createNewContainer(array(
-            'enabled' => true,
-            'page' => $global,
-            'code' => 'footer',
-        )));
-
-        $footer->setName('The footer container');
-
-        $footer->addChildren($text = $blockManager->create());
-
-        $text->setType('sonata.block.service.text');
-        $text->setSetting('content', <<<FOOTER
-        <a href="http://www.sonata-project.org">Sonata Project</a> sandbox demonstration.
-
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-25614705-2']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-FOOTER
-);
-        $text->setPosition(1);
-        $text->setEnabled(true);
-        $text->setPage($global);
-
         $pageManager->save($global);
     }
 
