bf791a9a4d49a8515ca7eda83105c50ca4355ee1
Thomas Rabaix <thomas.rabaix@sonata-project.org>
2013-11-13 05:30:08 -0500

    Cleanup

--- app/config/cache.rb
+++
@@ -1,9 +0,0 @@
-namespace :cache do
-    namespace :sonata do
-        task :purge, :roles => :app, :except => { :no_release => true } do
-            capifony_pretty_print "--> Purge Sonata pages cache"
-
-            run "cd #{latest_release} && #{php_bin} #{symfony_console} sonata:cache:flush-all"
-        end
-    end
-end
