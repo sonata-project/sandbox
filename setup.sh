db544232d80f1d53adcf28a4b448719b81bac01d
Armin Weihbold <armin.weihbold@gmail.com>
2013-12-13 19:51:31 +0100

    fix setup script

--- setup.sh
+++ setup.sh
@@ -1,10 +1,13 @@
+#!/bin/bash
+app/console doctrine:database:drop --force
 app/console doctrine:database:create
 app/console doctrine:schema:create
 app/console assets:install web
 app/console init:acl
 app/console sonata:admin:setup-acl
 app/console sonata:admin:generate-object-acl
-app/console sonata:page:create-site --enabled=true --name=localhost --host=localhost --relativePath=/ --enabledFrom=now --enabledTo="+10 years" --default=true
+#app/console sonata:page:create-site --enabled=1 --name=localhost --host=localhost --relativePath=/ --enabledFrom=now --enabledTo="+10 years" --default=true
+app/console doctrine:fixtures:load
 app/console sonata:page:update-core-routes --site=all
 app/console sonata:page:create-snapshots --site=all
-app/console doctrine:fixtures:load
+app/console assetic:dump
