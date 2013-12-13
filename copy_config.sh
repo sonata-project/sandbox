6c98466cd41e541e8e67d59220fb57949aee3aa4
Luis Cordova <cordoval@gmail.com>
2013-12-13 20:20:47 +0100

    remove unnecessary params

--- copy_config.sh
+++ copy_config.sh
@@ -1,3 +1,2 @@
 cp app/config/parameters.yml.sample app/config/parameters.yml
-cp app/config/parameters.yml.sample app/config/validation_parameters.yml
-cp app/config/parameters.yml.sample app/config/production_parameters.yml
+
