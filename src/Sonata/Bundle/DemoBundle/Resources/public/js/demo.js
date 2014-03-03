61348eab928d606fa9e45cea55b87a64d0b53d58
Sylvain Deloux <sylvain.deloux@gmail.com>
2014-03-03 17:11:51 +0000

    Added JS confirmation for un-updated basket

--- src/Sonata/Bundle/DemoBundle/Resources/public/js/demo.js
+++ src/Sonata/Bundle/DemoBundle/Resources/public/js/demo.js
@@ -17,4 +17,24 @@ $(document).ready(function () {
             }
         });
     });
+
+    if ($('.basket')) {
+        var update_basket = false;
+
+        $(".basket input[type=number]").on('change', function (e) {
+            update_basket = true;
+        });
+
+        $(".basket input[type=checkbox]").on('change', function (e) {
+            update_basket = true;
+        });
+
+        $(".sonata-basket-nextstep").on('click', function (e) {
+            if (update_basket) {
+                if (!confirm(basket_update_confirmation_message)) {
+                    e.preventDefault();
+                }
+            }
+        });
+    }
 });
