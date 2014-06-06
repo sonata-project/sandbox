22956ba77c93767e2c35ec724856812cffdbd4d5
Romain Mouillard <romain.mouillard@gmail.com>
2014-06-06 17:33:31 +0200

    Add junit export to behat tests

--- bin/qa_behat.sh
+++ bin/qa_behat.sh
@@ -6,12 +6,18 @@
 # For the full copyright and license information, please view the LICENSE
 # file that was distributed with this source code.
 
+if [ ! -d "build" ]; then
+    mkdir build
+fi
+
+parameters=" -f progress,junit --out ,build/junit"
+
 echo "Running Behat default test suite"
-bin/behat -f progress
+bin/behat -f progress $parameters
 statusDefault=$?
 
 echo "Running Behat API test suite"
-bin/behat --profile api -f progress
+bin/behat --profile api $parameters
 statusApi=$?
 
 if [ $statusDefault -ne 0 ] || [ $statusApi -ne 0 ]; then
