18f88967d6c7b2c9ffdf808185b3c5fc928553f1
Thomas Rabaix <thomas.rabaix@gmail.com>
2014-09-09 00:04:04 +0200

    improve Car Admin

--- src/Sonata/Bundle/DemoBundle/Admin/CarAdmin.php
+++ src/Sonata/Bundle/DemoBundle/Admin/CarAdmin.php
@@ -70,12 +70,24 @@ class CarAdmin extends Admin
     protected function configureFormFields(FormMapper $formMapper)
     {
         $formMapper
-            ->add('name')
-            ->add('engine', 'sonata_type_model_list')
-            ->add('rescueEngine')
-            ->add('inspections', 'sonata_type_collection', array('by_reference' => false), array('edit' => 'inline'))
-            ->add('createdAt')
-            ->add('color', 'sonata_type_model_list')
+            ->with('General', array('class' => 'col-md-6'))
+                ->add('name')
+                ->add('rescueEngine')
+                ->add('createdAt')
+            ->end()
+            ->with('Options', array('class' => 'col-md-6'))
+                ->add('engine', 'sonata_type_model_list')
+                ->add('color', 'sonata_type_model_list')
+            ->end()
+            ->with('inspections', array('class' => 'col-md-12'))
+                ->add('inspections', 'sonata_type_collection', array(
+                    'by_reference'       => false,
+                    'cascade_validation' => true,
+                ), array(
+                    'edit' => 'inline',
+                    'inline' => 'table'
+                ))
+            ->end()
         ;
     }
 
@@ -85,8 +97,7 @@ class CarAdmin extends Admin
     public function getNewInstance()
     {
         $object = parent::getNewInstance();
-
-
+        
         $inspection = new Inspection();
         $inspection->setDate(new \DateTime());
         $inspection->setComment("Initial inpection");
